7233be8a784ddd49cb4b812b4a5d494dc4e2a382
Ricardo Oliveira <153216922+ricardofoliveira61@users.noreply.github.com>
2024-04-07 23:58:49 +0100

    [UPDATE] script_primeira_parte.R

--- script_primeira_parte.R
+++ script_primeira_parte.R
@@ -1,14 +1,8 @@
-setwd("C:/Users/Utilizador/Desktop/Universidade/Bioinformática 1º ano/2º Semestre/Extração de Conhecimento de Dados Biológicos/Enunciado do trabalho 1")
-getwd()
-
 #instalar o pacote BiocManager caso ele não esteja já instalado
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-
-#instalar o pacote TCGAbiolinks caso ele não esteja já instalado
 if (!requireNamespace("TCGAbiolinks", quietly = TRUE))
   BiocManager::install("TCGAbiolinks")
-
 BiocManager::install("SummarizedExperiment", dependencies = TRUE)
 
 
@@ -19,7 +13,6 @@ library(SummarizedExperiment)
 
 
 #criar uma consulta ao Genomic Data Commons (GDC) e obter dados de perfilamento 
-#transcriptômico do projeto TCGA sobre carcinoma endometrial uterino (UCEC)
 projeto <- "TCGA-UCEC"
 query <- GDCquery(
   project = projeto,
@@ -28,20 +21,42 @@ query <- GDCquery(
   workflow.type = "STAR - Counts"
 )
 
+
 #baixar os dados do Genomic Data Commons (GDC) com base nas especificações 
-#de uma consulta criada anteriormente
 GDCdownload(query=query)
+
+
 #preparar os dados baixados do Genomic Data Commons (GDC) para análise
 rna_seq_UCEC  <- GDCprepare(query = query, save = TRUE, save.filename = "mRNA_TCGA-UCEC.rda")
+metadata(rna_seq_UCEC)
+
+
+#loading dos dados a partir dos ficheiros criados no GDCprepare
+rna_seq_UCEC= get(load("C:/Users/ricar/Downloads/mRNA_TCGA-UCEC.rda"))
+
 
 #retorna a classe do objeto (tipo de dados ou a estrutura de dados que o objeto 
-#representa, o que por sua vez determina quais funções podem ser aplicadas a ele)
 class(rna_seq_UCEC)
+
+
 #retorna as dimensões do objeto(matriz ou um dataframe;objeto mais complexo)
 dim(rna_seq_UCEC)
+colnames(rna_seq_UCEC)
+rownames(rna_seq_UCEC)
+row_metadados=SummarizedExperiment::rowData(rna_seq_UCEC);row_metadados[1:10,c('type','gene_type','gene_name')] # extarct metados relacionados com as linhas
+
+
+#atribui a um novo objeto chamado meta_UCEC, os metadados associados aos dados de expressão
+amostras_metadados = colData(rna_seq_UCEC)
+class(amostras_metadados)
+dim(amostras_metadados)
+colnames(amostras_metadados)
+
 
 #código utilizado para extrair a informação relacionada à contagem da expressão dos genes do objeto rna_seq_UCEC
 geneExp <- SummarizedExperiment::assay(rna_seq_UCEC)
+
+
 #para ver os outliers - no nosso caso é zero
 pre = TCGAanalyze_Preprocessing(rna_seq_UCEC) #faz correlação e tenta identificar outliers
 dim(geneExp)
@@ -51,19 +66,53 @@ dim(pre)
 #NOTA: o edger pode fazer outro processo de normalização através da função rpkm 
 #(tem que se fazer a normalização ou antes ou depois da analise diferencial)
 
-#Ver nomes das colunas
-colnames(rna_seq_UCEC)
-#nomes das linhas
-names(rna_seq_UCEC)
 
 
-#atribui a um novo objeto chamado meta_UCEC, os metadados associados ao conjunto 
-#de dados rna_seq_UCEC
-meta_UCEC = colData(rna_seq_UCEC)
+
+
 #retorna as dimensões dos metadados 
+class(meta_UCEC)
 dim(meta_UCEC)
+meta_UCEC= as.data.frame(meta_UCEC)
+
 #Ver nomes das colunas
+
 colnames(meta_UCEC)
+
+# criar vetor logica em que diz que linhas é que possuem grade diferente de na
+coluna=!is.na(meta_UCEC$paper_tumor_grade) 
+
+
+# seleciona apenas as linhas que tem grade
+meta=meta_UCEC[coluna,c("paper_tumor_grade","paper_age","disease_type")] 
+meta$disease_type
+table(meta_UCEC[rownames(meta),"paper_tumor_grade"]) # confirmar que os pacientes selecionados tem um valor de grade atribuido na tabela de metadados original
+head(meta)
+
+summary(meta$paper_age)
+boxplot(meta$paper_age,horizontal=T)
+
+anova=aov(meta$paper_age~meta$paper_tumor_grade)
+summary(anova)
+
+TukeyHSD(anova)
+
+boxplot(meta$paper_age~meta$paper_tumor_grade,horizontal=T)
+#verificaçáo da dimensão do dataframe meta após selação de apenas as linhas com informação relativa ao grade
+sum(table(meta_UCEC$paper_tumor_grade))
+dim(meta)
+
+
+#criação do data frame de expressão apenas para os pacientes com informação relativa ao grade
+exp_grade=geneExp[,rownames(meta)]
+dim(exp_grade)
+
+
+
+row.names(meta_UCEC[,"paper_tumor_grade"])
+select=meta_UCEC$paper_tumor_grade
+row_names=rownames(select)
+
 #nomes das linhas
 names(meta_UCEC)
 #extrair componentes de um objeto por nome (através de colunas)
