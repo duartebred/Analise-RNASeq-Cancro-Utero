2f4395987e305a0de1d22e44f18d8b092b3525f7
Ricardo <ricardofoliveira61@gmail.com>
2024-04-08 17:43:03 +0100

    [UPDATE] script_primeira_parte.r

--- script_primeira_parte.R
+++ script_primeira_parte.R
@@ -12,72 +12,83 @@ library(TCGAbiolinks)
 library(SummarizedExperiment)
 
 
-#criar uma consulta ao Genomic Data Commons (GDC) e obter dados de perfilamento 
-projeto <- "TCGA-UCEC"
-query <- GDCquery(
+# Realização de uma consulta ao Genomic Data Commons (GDC) para obter os dados de expressão referentes ao projeto em estudo
+query_TCGA_UCEC <- GDCquery(
   project = projeto,
   data.category = "Transcriptome Profiling", 
   data.type = "Gene Expression Quantification",
-  workflow.type = "STAR - Counts"
-)
+  workflow.type = "STAR - Counts")
 
 
-#baixar os dados do Genomic Data Commons (GDC) com base nas especificações 
-GDCdownload(query=query)
-
-
-#preparar os dados baixados do Genomic Data Commons (GDC) para análise
-rna_seq_UCEC  <- GDCprepare(query = query, save = TRUE, save.filename = "mRNA_TCGA-UCEC.rda")
-metadata(rna_seq_UCEC)
+# Download e preparação dos dados com base na query
+GDCdownload(query=query_TCGA_UCEC)
+rna_seq_UCEC  <- GDCprepare(query = query_TCGA_UCEC, save = TRUE, save.filename = "mRNA_TCGA-UCEC.rda")
 
 
 #loading dos dados a partir dos ficheiros criados no GDCprepare
 rna_seq_UCEC= get(load("C:/Users/ricar/Downloads/mRNA_TCGA-UCEC.rda"))
 
 
-#retorna a classe do objeto (tipo de dados ou a estrutura de dados que o objeto 
+# analise da estrutura dos dados descarregados
 class(rna_seq_UCEC)
-
-
-#retorna as dimensões do objeto(matriz ou um dataframe;objeto mais complexo)
 dim(rna_seq_UCEC)
 colnames(rna_seq_UCEC)
 rownames(rna_seq_UCEC)
-row_metadados=SummarizedExperiment::rowData(rna_seq_UCEC);row_metadados[1:10,c('type','gene_type','gene_name')] # extarct metados relacionados com as linhas
 
 
-#atribui a um novo objeto chamado meta_UCEC, os metadados associados aos dados de expressão
-amostras_metadados = colData(rna_seq_UCEC)
+# análise dos metadados
+linhas_metadados=SummarizedExperiment::rowData(rna_seq_UCEC)
+amostras_metadados = SummarizedExperiment::colData(rna_seq_UCEC)
+
+class(linhas_metadados)
 class(amostras_metadados)
+
+dim(linhas_metadados)
 dim(amostras_metadados)
+
+colnames(linhas_metadados)
 colnames(amostras_metadados)
 
 
-#código utilizado para extrair a informação relacionada à contagem da expressão dos genes do objeto rna_seq_UCEC
+# Extração da informação relacionada à contagem da expressão dos genes do objeto rna_seq_UCEC
 geneExp <- SummarizedExperiment::assay(rna_seq_UCEC)
 
 
-#para ver os outliers - no nosso caso é zero
-pre = TCGAanalyze_Preprocessing(rna_seq_UCEC) #faz correlação e tenta identificar outliers
-dim(geneExp)
-dim(pre)
+# Metadados das amostras
+## Análise exploratória
+not_reported = sapply(amostras_metadados,function(x) sum(x == "not reported", na.rm = TRUE))
+Not_Reported = sapply(amostras_metadados,function(x) sum(x == "Not Reported", na.rm = TRUE))
+nas = sapply(amostras_metadados,function(x) sum(is.na(x)))
+par(mfrow = c(1, 3))
+hist(not_reported[not_reported!=0],ylab="Número de colunas")
+hist(Not_Reported[Not_Reported!=0],ylab="Número de colunas")
+hist(nas[nas!=0],ylab="Número de colunas")
 
-#NOTA: a normalização pode ser realizada com o package deseq2 e com a função normalization
-#NOTA: o edger pode fazer outro processo de normalização através da função rpkm 
-#(tem que se fazer a normalização ou antes ou depois da analise diferencial)
+# remoção de colunas com mais de 10% de valores omissos
+rm_not_reported = which(sapply(amostras_metadados,function(x) sum(x == "not reported", na.rm = TRUE)) > 60)
+rm_Not_Reported = which(sapply(amostras_metadados,function(x) sum(x == "Not Reported", na.rm = TRUE)) > 60)
+rm_nas = which(sapply(amostras_metadados, function(x) sum(is.na(x))) > 60)
+amostras_meta = amostras_metadados[, -c(rm_not_reported, rm_Not_Reported, rm_nas)]
+
+
+colnames(metadata_matriz_clean)
+
+table(as.data.frame(metadata_matriz_clean$vital_status))
+summary(as.data.frame(metadata_matriz_clean$age_at_diagnosis))
+pie(table(as.data.frame(metadata_matriz_clean$primary_diagnosis)))
+summary(as.data.frame(metadata_matriz_clean$age_at_index))
+boxplot(as.data.frame(metadata_matriz_clean$age_at_index),horizontal=T)
+hist(metadata_matriz_clean$age_at_index)
+table(as.data.frame(metadata_matriz_clean$figo_stage))
+summary(as.data.frame(metadata_matriz_clean$figo_stage))
+sum(is.na(metadata_matriz_clean$figo_stage))
+dim(metadata_matriz_clean)
 
 
 
 
 
-#retorna as dimensões dos metadados 
-class(meta_UCEC)
-dim(meta_UCEC)
-meta_UCEC= as.data.frame(meta_UCEC)
 
-#Ver nomes das colunas
-
-colnames(meta_UCEC)
 
 # criar vetor logica em que diz que linhas é que possuem grade diferente de na
 coluna=!is.na(meta_UCEC$paper_tumor_grade) 
@@ -145,28 +156,7 @@ View(df)
 
 
 #######################################
-#Análise exploratória
-#Pré-processamento e filtragem
-#retirar as colunas dos metadados onde havia mais de 60 elementos como: 
-#“not/Not reported/Reported” e/ou “NA”
 
-cols_with_not_reported <- which(sapply(amostras_metadados,function(x) sum(x == "not reported", na.rm = TRUE)) > 60)
-cols_with_Not_Reported <- which(sapply(amostras_metadados,function(x) sum(x == "Not Reported", na.rm = TRUE)) > 60)
-cols_with_NA <- which(sapply(amostras_metadados, function(x) sum(is.na(x))) > 60)
-# remover as colunas baseadas nos critérios específicos de cima
-metadata_matriz_clean <- amostras_metadados[, -c(cols_with_not_reported, cols_with_Not_Reported, cols_with_NA)]
-colnames(metadata_matriz_clean)
-
-table(as.data.frame(metadata_matriz_clean$vital_status))
-summary(as.data.frame(metadata_matriz_clean$age_at_diagnosis))
-pie(table(as.data.frame(metadata_matriz_clean$primary_diagnosis)))
-summary(as.data.frame(metadata_matriz_clean$age_at_index))
-boxplot(as.data.frame(metadata_matriz_clean$age_at_index),horizontal=T)
-hist(metadata_matriz_clean$age_at_index)
-table(as.data.frame(metadata_matriz_clean$figo_stage))
-summary(as.data.frame(metadata_matriz_clean$figo_stage))
-sum(is.na(metadata_matriz_clean$figo_stage))
-dim(metadata_matriz_clean)
 
 
 
@@ -216,3 +206,13 @@ dea <- as.data.frame(res)
 #como o número de genes significativamente diferencialmente expressos
 summary(res)
 
+
+
+#para ver os outliers - no nosso caso é zero
+pre = TCGAanalyze_Preprocessing(rna_seq_UCEC) #faz correlação e tenta identificar outliers
+dim(geneExp)
+dim(pre)
+
+#NOTA: a normalização pode ser realizada com o package deseq2 e com a função normalization
+#NOTA: o edger pode fazer outro processo de normalização através da função rpkm 
+#(tem que se fazer a normalização ou antes ou depois da analise diferencial)
